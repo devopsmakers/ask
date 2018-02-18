5261de664b76f7698a9d3735128d3827f7e674de
Tim Birkett <tim.birkett@itv.com>
2018-02-18 22:04:22 +0000

    Refactor view (add base view), rename tags file

---
+++ qanda/templatetags/qanda_tags.py
@@ -0,0 +1,14 @@
+from django import template
+from django.utils.html import format_html
+register = template.Library()
+
+@register.simple_tag
+def count_badge(count=0, false_colour='info', true_colour='danger' ):
+    badge_colour = false_colour
+    if count > 0:
+        badge_colour = true_color
+
+    return format_html(
+        '&nbsp;<sup><span class="badge badge-pill badge-{}">{}</span></sup>',
+        badge_colour,
+        count)
--- qanda/views.py
+++ qanda/views.py
@@ -1,8 +1,9 @@
 from django.shortcuts import render
-from django.views.generic import FormView
+from django.views.generic import FormView, View
 from django.conf import settings
 
 from .forms import SearchForm
+from .models import Question
 
 try:
     qa_messages = 'django.contrib.messages' in settings.INSTALLED_APPS and \
@@ -15,9 +16,19 @@ if qa_messages:
     from django.contrib import messages
 
 # Create your views here.
+# QandaBaseView
+class QandaBaseView(View):
+    def get_context_data(self, **kwargs):
+        context = super(QandaBaseView, self).get_context_data(**kwargs)
+        context['ORGANIZATION_NAME'] = settings.QANDA_SETTINGS.get(
+            'organization', '')
+        context['LOGO'] =  settings.QANDA_SETTINGS.get(
+            'logo', False)
+        context['unanswered_count'] = Question.objects.unanswered().count()
+        return context
 
 # SearchView
-class SearchView(FormView):
+class SearchView(QandaBaseView, FormView):
     """ Main index / search view """
     template_name = 'qanda/index.html'
     form_class = SearchForm
@@ -25,10 +36,6 @@ class SearchView(FormView):
     def get_context_data(self, **kwargs):
         context = super(SearchView, self).get_context_data(**kwargs)
         context['nav'] = 'search'
-        context['ORGANIZATION_NAME'] = settings.QANDA_SETTINGS.get(
-            'organization', '')
-        context['LOGO'] =  settings.QANDA_SETTINGS.get(
-            'logo', False)
         return context
 
 
